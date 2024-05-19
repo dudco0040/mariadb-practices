31686397d1add192ae423e864fb5e6e7d4b1a4f6
BITCOM <BITCOM@BIT>
2024-05-19 21:08:42 +0900

    5/17 실습 업로드(Emaillist)

---
+++ jdbc-practices/src/test/java/emaillist/dao/EmailListDaoTest.java
@@ -0,0 +1,61 @@
+package emaillist.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import emaillist.vo.EmaillistVo;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EmailListDaoTest {
+	
+	private static int count = 0;
+	
+	@BeforeAll 
+	public static void setUp() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		count = list.size();
+	}	
+	
+	@Test
+	@Order(1)
+	public void testInsert() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("둘");
+		vo.setLastName("리");
+		vo.setEmail("dooly@gmail.com");
+		
+		boolean result = new EmaillistDao().insert(vo);
+		assertTrue(result); // true이면 명령어 사용을 성공함을 뜻함 
+		
+	}
+	@Test
+	@Order(2)
+	public void testFindByAll() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		assertEquals(count + 1, list.size());
+	}
+	
+	@Test 
+	@Order(3)
+	public void testDeleteByEmail() {
+		boolean result = new EmaillistDao().deleteByEmail("dooly@gmail.com");
+		assertTrue(result); 
+
+	}
+	
+	
+	@AfterAll
+	public static void cleanup() {
+		
+	}
+	
+}
