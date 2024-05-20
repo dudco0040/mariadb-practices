3e82f9bed42ecf1b42c14cd54c896cc8068c5524
BITCOM <BITCOM@BIT>
2024-05-20 19:40:39 +0900

    5/20 수업 실습 업로드(bookshop)

---
+++ jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
@@ -0,0 +1,51 @@
+package bookshop.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import bookshop.vo.AuthorVo;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AuthorDaoTest {
+	// Dao 객체 생성을 static 변수로 빼와서 사용 
+	private static int count = 0;
+	private static AuthorDao authorDao = new AuthorDao();
+	private static AuthorVo mockAuthorVo = new AuthorVo();
+
+	@BeforeAll
+	public static void setUp() {
+		count = authorDao.findAll().size();
+	}
+	
+	@Test
+	@Order(1)
+	public void testInsert() {
+		mockAuthorVo.setName("칼세이건");
+		
+		boolean result = authorDao.insert(mockAuthorVo);
+		// assertTrue(result);
+		System.out.println(mockAuthorVo);
+		assertNull(mockAuthorVo.getNo());
+	}
+	
+	@Test
+	@Order(2)
+	public void testFindAll() {
+		assertEquals(count +1, authorDao.findAll().size());
+		
+	}
+	
+	@AfterAll
+	public static void cleanUp() {
+		authorDao.deleteByNo(mockAuthorVo.getName());
+	}
+}
