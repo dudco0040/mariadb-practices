b4e9f6a76a13fbee12d3e5f88d38b5229c5f9dd8
BITCOM <BITCOM@BIT>
2024-05-22 01:44:58 +0900

    bookmall SQL 쿼리 테스트

---
+++ sql 연습/bookmall 테스트 쿼리.sql
@@ -0,0 +1,30 @@
+select * from user;
+select * from category;
+select * from book;
+select * from cart;
+select * from orders;
+select * from order_book;
+
+desc cart;
+
+delete from user;
+
+
+delete from user where no = 54;
+
+select user_no, book_no, quantity from cart where user_no = 1;
+
+select a.user_no, b.book_no, c.title, b.quantity, b.price
+from orders a, order_book b, book c
+where a.no = b.order_no 
+  and b.book_no = c.no
+  and a.no = ?
+  and a.user_no =? ;
+
+select a.book_no, b.title, a.user_no, a.order_no, a.quantity, a.price
+from ( select b.book_no, a.user_no, b.order_no, b.quantity, b.price from orders a, order_book b where a.no = b.order_no and a.no = ? and a.user_no =? ) a, book b
+where a.book_no = b.no;
+
+
+
+select b.book_no, a.user_no, b.order_no, b.quantity, b.price from orders a, order_book b where a.no = b.order_no and a.no = ? and a.user_no =?
