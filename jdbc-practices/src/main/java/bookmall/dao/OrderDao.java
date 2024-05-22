31a3001fffde8f21451508bc64d4a8e637cde7d3
BITCOM <BITCOM@BIT>
2024-05-22 19:51:51 +0900

    bookmall 과제 업로드(done)

--- jdbc-practices/src/main/java/bookmall/dao/OrderDao.java
+++ jdbc-practices/src/main/java/bookmall/dao/OrderDao.java
@@ -148,30 +148,39 @@ public class OrderDao {
 		
 		try (
 				Connection conn = getConnection();
-				PreparedStatement pstmt = conn.prepareStatement("select b.book_no, a.user_no, b.order_no, b.quantity, b.price from orders a, order_book b where a.no = b.order_no and a.no = ? and a.user_no =? " );
+				PreparedStatement pstmt = conn.prepareStatement("select a.user_no, b.book_no, c.title, b.quantity, b.price, b.order_no"
+						+ " from orders a, order_book b, book c"
+						+ " where a.no = b.order_no"
+						+ " and b.book_no = c.no"
+						+ " and a.no = ? and a.user_no = ?" );
 				
 				// title 값을 가져오려면 book no 값을 가지고 와야 함 -> book 테이블과도 조인이 필요
 				// 조인했을때, sql 문에 오류가 있는 것으로 보임
 				// 우선, OrderBookVo에 BookTitle 변수는 추가했고, sql 문이랑 이 코드의 ResultSet에 저장하는 부분만 변경하면 됨 
 			){
 				//binding
+
 				pstmt.setLong(1, get_no);
 				pstmt.setLong(2, get_userNo);
 				ResultSet rs = pstmt.executeQuery();
 				
+				System.out.println(get_no);
+				System.out.println(get_userNo);
 				while(rs.next()) {
-					Long book_no = rs.getLong(1);
-					Long user_no = rs.getLong(2);
-					Long order_no = rs.getLong(3); 
+					Long user_no = rs.getLong(1);
+					Long book_no = rs.getLong(2);
+					String book_title = rs.getString(3);
 					int quantity = rs.getInt(4);
 					int price  = rs.getInt(5);
+					Long order_no = rs.getLong(6);
 					
 					OrderBookVo vo = new OrderBookVo();
-					vo.setBookNo(book_no);
 					vo.setUserNo(user_no);
-					vo.setOrderNo(order_no);
+					vo.setBookNo(book_no);
+					vo.setBookTitle(book_title);
 					vo.setQuantity(quantity);
 					vo.setPrice(price);
+					vo.setOrderNo(order_no);
 					
 					result.add(vo);
 			
